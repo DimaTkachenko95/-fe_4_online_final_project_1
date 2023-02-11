f86c4bb96be4b187d50f21f038cb2e84a75714ee
toni.kiselevich <toni.kiselevich@gmail.com>
2023-02-11 22:05:02 +0200

    deleted files

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
-import Positions from "./Components/Pages/Products";
-import Products from "./Components/Pages/Products";
-import Main from "./Components/Pages/Main";
-import Basket from "./Components/Pages/Backet";
-import CheckOut from "./Components/Pages/CheckOut";
-import Contacts from "./Components/Pages/Contacts";
-import Favorites from "./Components/Pages/Favorites";
-import Product from "./Components/Pages/Product";
+import Products from "./Pages/Products";
+import Main from "./Pages/Main";
+import Basket from "./Pages/Backet";
+import CheckOut from "./Pages/CheckOut";
+import Contacts from "./Pages/Contacts";
+import Favorites from "./Pages/Favorites";
+import Product from "./Pages/Product";
 
 const App = () =>  {
 
