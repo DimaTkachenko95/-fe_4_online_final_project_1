0db0cdbf2c9b62aa907c3248021664d014d5275f
toni.kiselevich <toni.kiselevich@gmail.com>
2023-02-23 00:24:03 +0200

    fixed search & connected w back-end

--- client/src/components/InputSearch/InputSearch.jsx
+++ client/src/components/InputSearch/InputSearch.jsx
@@ -1,22 +1,23 @@
 import { IconButton, InputBase } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
-import { actionFetchAllProducts, actionSearchProducts } from "../../reducers";
+import { actionSearchProducts } from "../../reducers";
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectorAllProducts } from "../../selectors";
+import { useDispatch } from "react-redux";
+
+import axios from 'axios';
 
 const InputSearch = ({style = "header__input"}) => {
     const [inputValue, setInputValue] = useState('');
-    const allProducts = useSelector(selectorAllProducts);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleSearch = () => {
-        dispatch(actionFetchAllProducts());
-        const searchProducts = allProducts.filter(product => product.name.toLowerCase().includes(inputValue.toLowerCase()));
+    const handleSearch = async () => {
+        const result = await axios.post("http://localhost:5000/api/products/search", {query: inputValue});
+        const searchProducts = result.data;
         dispatch(actionSearchProducts(searchProducts));
         setInputValue('')
+
     }
 
     const handleEnterPress = (event) => {
