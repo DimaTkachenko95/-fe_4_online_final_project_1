ba9d584e9db2f4a62e2a6fb30ef89bb2cabc397d
Alina Holovko <alina.gerashchenko.91@gmail.com>
2023-02-22 22:05:51 -0500

    added styles for hover to Btn

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -5,25 +5,62 @@ import classnames from 'classnames';
 
 const mixin = (props) => {
     switch (props.variant) {
-      case 'gradiant-green':
+      case 'gradient-green':
         return css`
-          color: #ffffff;
+          && {color: #ffffff;
           background: linear-gradient(#8EE902, #4F9C2C);
           box-shadow: 0px 5px 6px rgba(79, 156, 44, 0.6);
+            &:hover {
+              ${hoverGradientGreen}
+            }}
         `;
       case 'white-shadow':
         return css`
-          color: #000000;
+          && {color: #000000;
           background: #ffffff;
           box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+           &:hover {
+             ${hoverWhiteShadow}
+           }}
         `;
+        case 'pale-green': 
+         return css`
+            && {background: #d6e3d1;
+            color: #4d9546;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;}
+         `
+
       default:
         return css`
-          color: #000000;
-          background: linear-gradient(#8EE902, #4F9C2C);
+          && {color: rgb(245, 240, 240);
+          background: #4ba123;
+           &:hover {
+            ${hoverGreen}
+           }}
         `;
     }
   };
+
+  const hoverGreen = css`
+    background: #d6e3d1;
+    color: #4d9546;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  `
+  
+  const hoverGradientGreen = css`
+    background: linear-gradient(to bottom, #FFFFFF, #80808080);
+    color: #4d9546;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  `
+
+  const hoverWhiteShadow = css`
+    box-shadow: 0px 3px 8px rgba(79, 156, 44, 0.6);
+    background: linear-gradient(#FFFFFF, rgba(79, 156, 44, 0.6));
+    color: #4d9546;
+    //background: linear-gradient(to bottom, #FFFFFF, #D3D3D3);
+  `
+
+  //background: linear-gradient(to bottom, #f5f5f5, #d3d3d3);
 
   const widthMixin = css`
   && {
@@ -31,6 +68,9 @@ const mixin = (props) => {
   }
 `;
 
+        // background-color: #d6e3d1;
+        // color: #4d9546;
+        // box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
 
   const ButtonMain = styled(ButtonBase).attrs((props) => ({
     href: props.href,
@@ -56,6 +96,7 @@ const Btn = ({text, type, variant, width, href, to, className, ...restProps}) =>
 
     const buttonClassName = classnames({
         'gradient-green': variant === 'gradient-green',
+        'pale-green': variant === 'pale-green',
         'white-shadow': variant === 'white-shadow'
       }, className);
     
