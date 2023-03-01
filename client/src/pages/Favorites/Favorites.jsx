02cac5dddc8efe7f140382637566bbee807a128d
DimaTkachenko95 <119577366+DimaTkachenko95@users.noreply.github.com>
2023-03-01 20:11:08 +0200

    добавил картинкиadded pictures when out of stock

--- client/src/pages/Favorites/Favorites.jsx
+++ client/src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,4 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import './Favorites.scss';
 import {useDispatch, useSelector} from "react-redux";
 import {
@@ -16,6 +11,8 @@ import {actionFetchProductFavoritesByItemNo} from "../../reducers/favorites.redu
 import ProductCard from "../../components/ProductCard";
 
 import Grid from "@mui/material/Grid";
+import {useState} from "react";
+import EmptyFavorites from "./EmptyFavorites";
 
 
 export default function Favorites() {
@@ -23,8 +20,19 @@ export default function Favorites() {
   const favorites = useSelector(selectorFavorites)
     const productFavorites = useSelector(selectorFavoritesProduct);
     const dispatch = useDispatch();
+    const [isEmpty, setIsEmpty] = useState(true);
+
+    // useEffect(() => {
+    //     if (favorites.length >= 1) {
+    //         setIsEmpty(false)
+    //     } else setIsEmpty(true)
+    // }, [favorites])
+
 
     useEffect(() => {
+        if (favorites.length >= 1) {
+            setIsEmpty(false)
+        } else setIsEmpty(true)
 
         dispatch(actionFetchProductFavoritesByItemNo(favorites))
 
@@ -33,13 +41,16 @@ export default function Favorites() {
 
   return (
       <div className="sector_favorites">
+          {isEmpty ?
+              <EmptyFavorites/> :
                                   <Grid container spacing={4}>
                                       {productFavorites.map((el, index) => (
-                                          <Grid className="grid-main-list" item xs="12" sm="6" md="4" key={el._id}>
+                                          <Grid className="grid-main-list" item xs="12" sm="5" md="3" key={el._id}>
                                               <ProductCard el={el} index={index} />
                                           </Grid>
                                       ))}
                                   </Grid>
+          }
       </div>
   )
 }
