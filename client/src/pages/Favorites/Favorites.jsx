d7babec44bc37e2c02d2f5328e565cf8408d8140
DimaTkachenko95 <119577366+DimaTkachenko95@users.noreply.github.com>
2023-03-03 13:41:15 +0200

    fix

--- client/src/pages/Favorites/Favorites.jsx
+++ client/src/pages/Favorites/Favorites.jsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import './Favorites.scss';
 import {useDispatch, useSelector} from "react-redux";
-import {
-    selectorFavorites,
-    selectorFavoritesProduct,
-} from "../../selectors";
+import {selectorFavorites, selectorFavoritesProduct,} from "../../selectors";
 import {useEffect} from "react";
-
 import {actionFetchProductFavoritesByItemNo} from "../../reducers/favorites.reducer";
 import ProductCard from "../../components/ProductCard";
-
 import Grid from "@mui/material/Grid";
 import EmptyFavorites from "./EmptyFavorites";
+import { Container } from '@mui/material';
 
 
 export default function Favorites() {
@@ -29,16 +25,22 @@ export default function Favorites() {
 
   return (
       <div className="sector_favorites">
+          <Container maxWidth="lg">
           {favorites.length <= 1 ?
               <EmptyFavorites/> :
-                                  <Grid  container xs={12} spacing={3}>
-                                      {productFavorites.map((el, index) => (
-                                          <Grid className="grid_favorites" item  key={el._id}>
-                                              <ProductCard el={el} index={index} />
-                                          </Grid>
-                                      ))}
-                                  </Grid>
+              <div>
+                  <Grid container spacing={12}>
+                      {productFavorites.map((el, index) => (
+                          <Grid className="grid-main-list" item xs="12" sm="6" md="4" key={el._id}>
+                              <ProductCard el={el} index={index} />
+                          </Grid>
+                      ))}
+                  </Grid>
+              </div>
           }
+          </Container>
       </div>
+
   )
 }
+
