de902341136c9f55b36701e95c1673abaa5c4630
Bohdan Kletskyi <underwater3499@gmail.com>
2023-03-05 00:21:36 +0200

    redy_to_marge

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import {Box, Container, createTheme, ThemeProvider} from '@mui/material';
+import { Box, Container, createTheme, ThemeProvider } from '@mui/material';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import {ReactComponent as ScaleSvg} from './icons/scales-of-justice-svgrepo-com.svg';
+import { ReactComponent as ScaleSvg } from './icons/scales-of-justice-svgrepo-com.svg';
 import './Header.scss';
 import { useEffect, useRef, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
@@ -13,15 +13,15 @@ import { useSelector} from 'react-redux';
 import InputSearch from '../InputSearch';
 
 const theme = createTheme({
-    components: {
-        MuiContainer: {
-            styleOverrides: {
-                root: {
-                    maxWidth: '1800px', // Set your custom maxWidth value here
-                },
-            },
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          maxWidth: '1800px', // Set your custom maxWidth value here
         },
+      },
     },
+  },
 });
 
 const Header = () => {
@@ -31,20 +31,19 @@ const Header = () => {
   const scales = useSelector(selectorScales);
   const countInBasket = basket.reduce((acc, { cartQuantity }) => acc + cartQuantity, 0);
 
+  useEffect(() => {
+    document.addEventListener('mousedown', handleBurgerMenu);
+    return () => {
+      document.removeEventListener('mousedown', handleBurgerMenu);
+    };
+  }, []);
+  const burgerMenuRef = useRef();
 
-    useEffect(() => {
-        document.addEventListener("mousedown", handleBurgerMenu);
-        return (() => {
-            document.removeEventListener("mousedown", handleBurgerMenu);
-        })
-    },[])
-    const burgerMenuRef = useRef();
-
-    const handleBurgerMenu = (event) => {
-        if (burgerMenuRef && !burgerMenuRef.current.contains(event.target) && isMenuOpen) {
-            setIsMenuOpen(!isMenuOpen);
-        }
+  const handleBurgerMenu = (event) => {
+    if (burgerMenuRef && !burgerMenuRef.current.contains(event.target) && isMenuOpen) {
+      setIsMenuOpen(!isMenuOpen);
     }
+  };
 
   return (
     <>
@@ -59,7 +58,7 @@ const Header = () => {
                 </Link>
               </Box>
 
-              <nav className={isMenuOpen ? 'header__menu active' : 'header__menu'} ref={burgerMenuRef}>
+              <nav className={isMenuOpen ? 'header__menu header__menu--active' : 'header__menu'} ref={burgerMenuRef}>
                 <Box className="menu-list">
                   <NavLink
                     to="/products"
@@ -99,54 +98,54 @@ const Header = () => {
                 </Box>
               </nav>
 
-                            <Box className="header__input-wrapper">
-                                <InputSearch style="header__input"/>
-                            </Box>
-                            <Box className="header__items-actions">
-                                <Box className="action">
-                                    <div className="count">
-                                        <span>{favorites.length}</span>
-                                    </div>
-                                    <NavLink to="/favorites" className="action__icon icon-favorite">
-                                        <StarBorderIcon/>
-                                    </NavLink>
-                                </Box>
-                                <Box className="action">
-                                    <div className="count">
-                                        <span>{scales.length}</span>
-                                    </div>
-                                    <NavLink to="/compare" className="action__icon icon-compare">
-                                        <ScaleSvg/>
-                                    </NavLink>
-                                </Box>
-                                <Box className="action ">
-                                    <div className="count count-cart">
-                                        <span>{countInBasket}</span>
-                                    </div>
-                                    <NavLink to="/basket" className="action__icon icon-cart">
-                                        <ShoppingCartOutlinedIcon/>
-                                    </NavLink>
-                                </Box>
-                            </Box>
+              <Box className="header__input-wrapper">
+                <InputSearch style="header__input" />
+              </Box>
+              <Box className="header__items-actions">
+                <Box className="action">
+                  <div className="count">
+                    <span>{favorites.length}</span>
+                  </div>
+                  <Link to="/favorites" className="action__icon icon-favorite">
+                    <StarBorderIcon />
+                  </Link>
+                </Box>
+                <Box className="action">
+                  <div className="count">
+                    <span>{scales.length}</span>
+                  </div>
+                  <Link to="/compare" className="action__icon icon-compare">
+                    <ScaleSvg />
+                  </Link>
+                </Box>
+                <Box className="action ">
+                  <div className="count count-cart">
+                    <span>{countInBasket}</span>
+                  </div>
+                  <Link to="/basket" className="action__icon icon-cart">
+                    <ShoppingCartOutlinedIcon />
+                  </Link>
+                </Box>
+              </Box>
 
-                            <Box className="header__user-actions">
-                                <Box className="action">
-                                    {
-                                        <a href="#" className="action__icon icon-user">
-                                            <Person2OutlinedIcon/>
-                                        </a>
-                                    }
-                                </Box>
-                            </Box>
-                            <Box className="burger-menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                                {isMenuOpen ? <CloseOutlinedIcon/> : <MenuOutlinedIcon/>}
-                            </Box>
-                        </Box>
-                    </Container>
-                </ThemeProvider>
-            </header>
-        </>
-    );
+              <Box className="header__user-actions">
+                <Box className="action">
+                  {
+                    <a href="#" className="action__icon icon-user">
+                      <Person2OutlinedIcon />
+                    </a>
+                  }
+                </Box>
+              </Box>
+              <Box className="burger-menu-btn" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? <CloseOutlinedIcon /> : <MenuOutlinedIcon />}
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </header>
+    </>
+  );
 };
 
 export default Header;
