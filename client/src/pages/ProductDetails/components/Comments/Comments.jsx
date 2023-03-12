3ab0e494388ba8cfff3b3860047a8737beff6fea
cloutnba <cloutboy2@protonmail.com>
2023-03-12 20:55:55 +0200

    removed comment field for non-auth users

--- client/src/pages/ProductDetails/components/Comments/Comments.jsx
+++ client/src/pages/ProductDetails/components/Comments/Comments.jsx
@@ -1,7 +1,7 @@
 import {Box, Button, TextField} from "@mui/material";
 import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {selectorProduct, selectorProductComments} from "../../../../selectors";
+import {selectorProduct, selectorProductComments, selectorToken} from "../../../../selectors";
 import './Comments.scss';
 import {actionFetchAddComment, actionFetchAllComments} from "../../../../reducers/productDetails.reducer";
 import Comment from "./Comment";
@@ -11,6 +11,7 @@ import comment from "./Comment";
 const Comments = () => {
     const dispatch = useDispatch();
     const product = useSelector(selectorProduct);
+    const token = useSelector(selectorToken);
 
     const comments = useSelector(selectorProductComments);
 
@@ -19,42 +20,6 @@ const Comments = () => {
     }, [product]);
 
 
-
-
-    const MOC = {
-        "_id": "5d90e8fcea6f09306470adb9",
-        "customer": {
-            "isAdmin": false,
-            "enabled": true,
-            "_id": "5d63a92afc004f2e041179cd",
-            "firstName": "Kiril",
-            "lastName": "Kirilov",
-            "login": "test12",
-            "email": "test3@gmail.com",
-            "password": "$2a$10$Tfnzth419TINXXQB8pjiYevZCeKdDZmlL1o43k3guhnmiLgMcUkwG",
-            "date": "2019-08-26T09:40:58.667Z",
-            "__v": 0
-        },
-        "product": {
-            "enabled": true,
-            "imageUrls": ["products/itemNo2"],
-            "quantity": 40,
-            "_id": "5d73adb9fcad90130470f08f",
-            "name": "test product 5",
-            "currentPrice": 400,
-            "categories": "computers",
-            "someOtherFeature": "test5",
-            "color": "green",
-            "size": "xl",
-            "ram": "600",
-            "weight": "1800g",
-            "itemNo": "563877",
-            "__v": 0,
-            "date": "2019-10-22T17:05:21.426Z"
-        },
-        "content": "Very good product. Thank you so much !",
-        "__v": 0
-    }
 
     const [review, setReview] = useState('');
 
@@ -85,7 +50,7 @@ const Comments = () => {
     return (
         <>
             <Box className="product__comments-wrapper">
-                <Box className="product__input-wrapper">
+                {token && <Box className="product__input-wrapper">
                     <h3 className="product__comments-title">Leave your feedback:</h3>
                     <form onSubmit={handleSubmit} className="product__form">
                         <TextField
@@ -105,7 +70,7 @@ const Comments = () => {
                             Send
                         </Button>
                     </form>
-                </Box>
+                </Box>}
 
                 <Box className="product__comments-content">
                     <h3 className="product__comments-title">Reviews:</h3>
