a971a5e7379aa9e2dd90946a3b7fcbb78693189a
Anastasiia Shulzhenko <shulzhenko.anastasiia98@gmail.com>
2023-03-13 19:21:05 +0200

    fix slider

--- client/src/components/ProductsSlider/ProductsSlider.jsx
+++ client/src/components/ProductsSlider/ProductsSlider.jsx
@@ -10,63 +10,62 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 const ProductsSlider = ({ products }) => {
   
   const NextArrow = ({ className, onClick }) => {
-  return (
-    <div className={className} onClick={onClick}>
-      <NavigateNextIcon style={{color: 'black'}}/>
-    </div>
-  );
-}
+    return (
+      <div className={className} onClick={onClick}>
+        <NavigateNextIcon style={{color: 'black'}}/>
+      </div>
+    );
+  }
 
-const PrevArrow = ({ className, onClick }) => {
-  return (
-    <div className={className} onClick={onClick}>
-      <NavigateBeforeIcon style={{color: 'black'}}/>
-    </div>
-  );
-}
+  const PrevArrow = ({ className, onClick }) => {
+    return (
+      <div className={className} onClick={onClick}>
+        <NavigateBeforeIcon style={{color: 'black'}}/>
+      </div>
+    );
+  }
 
   const settings = {
-      className: "discounted-products__slider",
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      initialSlide: 0,
-      nextArrow: <NextArrow />,
-      prevArrow: <PrevArrow />,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-          }
-        },
-        {
-          breakpoint: 1000,
-          settings: {
-            slidesToShow: 2,
-            arrows: false,
-          }
-        },
-        {
-          breakpoint: 630,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-          }
+    className: "discounted-products__slider",
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
         }
-      ]
-    };
-return(
-  <>
-  {!!products.length &&
-    (
-<Container maxWidth="lg" className="discounted-products">
-<h2 className="discounted-products__title"><span className="discounted-products__title-colored">DISCOUNTED</span> PRODUCTS</h2>
-  <Container className="discounted-products__slider-container" maxWidth="lg">
-     
-        <Slider {...settings}>
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
+      }
+    ]
+  };
+  return(
+    <>
+      {!!products.length &&
+      (
+      <Container maxWidth="lg" className="discounted-products">
+        <h2 className="discounted-products__title"><span className="discounted-products__title-colored">DISCOUNTED</span> PRODUCTS</h2>
+        <Container className="discounted-products__slider-container" maxWidth="lg">
+          <Slider {...settings}>
           {products?.map((el, index) => {
           return (
             <div className="discounted-products__slider-item">
@@ -74,15 +73,14 @@ return(
               />
             </div>
           )
-        })}
-        </Slider>
-      
-  </Container>
-  </Container>
-  )
+          })}
+          </Slider>
+        </Container>
+      </Container>
+      )
       }
-      </>
-)
+    </>
+  )
 }
 
 export default ProductsSlider;
