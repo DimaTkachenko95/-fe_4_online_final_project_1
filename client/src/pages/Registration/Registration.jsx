80e7d7bfcccdb5b2febc30ba5f2d879706d4f041
Melnyk Anastasiia <nastasia8811@gmail.com>
2023-03-20 11:25:05 +0100

    rewrote registration reducer

--- client/src/pages/Registration/Registration.jsx
+++ client/src/pages/Registration/Registration.jsx
@@ -1,17 +1,26 @@
 import { Container } from '@mui/material';
-
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import './Registration.scss';
 import BreadCrumbs from '../../components/BreadCrumbs';
-import initialState from '../../reducers/registration.reducer';
-import { createCustomerServerApi } from '../../reducers';
-/* import FormComponent from './FormComponent/FormComponent'; */
+import {createCustomerServerApi, registrationIsLoading} from '../../reducers';
 import FormRegistration from '../../components/FormRegistration';
-
-
+import ValidationSchema from './components/ValidationSchema';
+import ModalSuccessRegistration from './components/ModalSuccessRegistration';
+import Preloader from '../../components/Preloader'
+import {useDispatch} from "react-redux";
+import {initialState} from '../../reducers'
 
 const Registration = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    //const [loading, setLoading] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+
+    const toggleModal = () => {
+        setOpenModal(!openModal);
+    };
+    const closeModal = () => {
+        setOpenModal(false);
+    };
 
   return (
     <main>
@@ -19,14 +28,24 @@ const Registration = () => {
         <BreadCrumbs linksArray={[{ link: '/registration', text: 'Registration' }]} />
         <div className="registration-container__wrapper">
           <h2 className="registration-container__wrapper-title">Registration</h2>
-          < FormRegistration 
-                  initialValues={initialState} 
-                  onSubmit={(values, { resetForm }) => {
-                      console.log(values)
-                      dispatch(createCustomerServerApi(values));
-                      resetForm();
-                  }} 
-                  inputsEditName={["firstName", "lastName", "login", "email", "password", "telephone", "gender", "avatarUrl"]}/>
+
+          <FormRegistration
+              initialValues={initialState}
+              validationSchema={ValidationSchema}
+              onSubmit={(values, { resetForm }) => {
+                  delete values.confirmPassword;
+                  dispatch(registrationIsLoading(true))
+                  createCustomerServerApi.then((savedCustomer) => {
+                      if (savedCustomer) {
+                          resetForm();
+                          toggleModal();}
+                      dispatch(registrationIsLoading(false))
+                  });
+              }}
+                  inputsEditName={["firstName", "lastName", "login", "email", "password", "telephone", "gender", "avatarUrl"]}
+          />
+
+            {openModal && <ModalSuccessRegistration closeModal={() => closeModal()} />}
         </div>
       </Container>
     </main>
