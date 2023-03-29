58580e474dafb2b58acb4447fa8bd8cd74f62ac2
Melnyk Anastasiia <nastasia8811@gmail.com>
2023-03-29 23:57:34 +0200

    configurations added

--- client/src/__tests__/formRegistrationTest.js
+++ client/src/__tests__/formRegistrationTest.js
@@ -1,81 +1,83 @@
-// describe('Should check test for eslint', () => {
-//   test('sss', () => {
-//     expect(123).toBe(123);
+// // describe('Should check test for eslint', () => {
+// //   test('sss', () => {
+// //     expect(123).toBe(123);
+// //   });
+// // });
+//
+// import React from 'react';
+// import { mount, shallow } from 'enzyme';
+// import { Formik } from 'formik';
+// import FormRegistration from './../components/FormRegistration';
+// import * as yup from 'yup';
+//
+//
+// describe('FormRegistration', () => {
+//   const initialValues = {
+//     firstName: '',
+//     lastName: '',
+//     login: '',
+//     email: '',
+//     password: '',
+//     confirmPassword: '',
+//     telephone: '',
+//     city: '',
+//     country: '',
+//   };
+//   const onSubmit = jest.fn();
+//   const validationSchema = null;
+//
+//   it('should render the component without errors', () => {
+//     const wrapper = mount(
+//         <FormRegistration
+//             onSubmit={onSubmit}
+//             initialValues={initialValues}
+//             validationSchema={validationSchema}
+//             btnEdit
+//             inputsEditName={['firstName']}
+//             withPassword
+//         />
+//     );
+//     expect(wrapper.exists()).toBeTruthy();
+//   });
+//
+//   it('should submit the form when the submit button is clicked', () => {
+//     const wrapper = mount (
+//         <FormRegistration
+//             onSubmit={onSubmit}
+//             initialValues={initialValues}
+//             validationSchema={validationSchema}
+//             btnEdit
+//             inputsEditName={['firstName']}
+//             withPassword
+//         />
+//     );
+//     wrapper.find(Formik).simulate('submit');
+//     expect(onSubmit).toHaveBeenCalled();
+//   });
+//
+//   it('should not allow the form to be submitted when there are validation errors', () => {
+//     const validationSchema = {
+//       firstName: yup.string().required('Required'),
+//       lastName: yup.string().required('Required'),
+//       login: yup.string().required('Required'),
+//       email: yup.string().email('Invalid email').required('Required'),
+//       password: yup.string().required('Required'),
+//       confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords do not match').required('Required'),
+//       telephone: yup.string().required('Required'),
+//       city: yup.string().required('Required'),
+//       country: yup.string().required('Required'),
+//     };
+//     const wrapper = mount (
+//         <FormRegistration
+//             onSubmit={onSubmit}
+//             initialValues={initialValues}
+//             validationSchema={validationSchema}
+//             btnEdit
+//             inputsEditName={['firstName']}
+//             withPassword
+//         />
+//     );
+//     wrapper.find(Formik).simulate('submit');
+//     expect(onSubmit).not.toHaveBeenCalled();
 //   });
 // });
-
-import React from 'react';
-import { mount } from 'enzyme';
-import { Formik } from 'formik';
-import FormRegistration from './../components/FormRegistration';
-
-describe('FormRegistration', () => {
-  const initialValues = {
-    firstName: '',
-    lastName: '',
-    login: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    telephone: '',
-    city: '',
-    country: '',
-  };
-  const onSubmit = jest.fn();
-  const validationSchema = null;
-
-  it('should render the component without errors', () => {
-    const wrapper = mount(
-        <FormRegistration
-            onSubmit={onSubmit}
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            btnEdit
-            inputsEditName={['firstName']}
-            withPassword
-        />
-    );
-    expect(wrapper.exists()).toBeTruthy();
-  });
-
-  it('should submit the form when the submit button is clicked', () => {
-    const wrapper = mount(
-        <FormRegistration
-            onSubmit={onSubmit}
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            btnEdit
-            inputsEditName={['firstName']}
-            withPassword
-        />
-    );
-    wrapper.find(Formik).simulate('submit');
-    expect(onSubmit).toHaveBeenCalled();
-  });
-
-  it('should not allow the form to be submitted when there are validation errors', () => {
-    const validationSchema = {
-      firstName: Yup.string().required('Required'),
-      lastName: Yup.string().required('Required'),
-      login: Yup.string().required('Required'),
-      email: Yup.string().email('Invalid email').required('Required'),
-      password: Yup.string().required('Required'),
-      confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Passwords do not match').required('Required'),
-      telephone: Yup.string().required('Required'),
-      city: Yup.string().required('Required'),
-      country: Yup.string().required('Required'),
-    };
-    const wrapper = mount(
-        <FormRegistration
-            onSubmit={onSubmit}
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            btnEdit
-            inputsEditName={['firstName']}
-            withPassword
-        />
-    );
-    wrapper.find(Formik).simulate('submit');
-    expect(onSubmit).not.toHaveBeenCalled();
-  });
-});
