d4d7b0c7ecdafae58eeb3ac548eee59d262eab54
Melnyk Anastasiia <nastasia8811@gmail.com>
2023-04-06 10:31:51 +0200

    framer-motion installed ScopedCssBaseline added reset.css removed

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import ScrollToTop from './helpers/scrollToTop';
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <ScrollToTop />
+        <ScopedCssBaseline>
       <App />
+        </ScopedCssBaseline>
     </BrowserRouter>
   </Provider>,
 );
